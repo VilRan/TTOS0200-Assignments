ff5191838e7b2be9de440b41bc9653b761775cf0
VilRan <VilleMattiRantala@gmail.com>
2016-01-19 18:47:15 +0200

    +Sauna assignment done.

---
+++ Basics/Objects/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter assignment number (1-1) or enter something else to exit > ");
+                int assignment;
+                if (!int.TryParse(Console.ReadLine(), out assignment)
+                    || assignment < 1 || assignment > 20)
+                    break;
+
+                Type type = typeof(Program);
+                MethodInfo info = type.GetMethod("Assignment" + assignment, BindingFlags.Static | BindingFlags.Public);
+                info.Invoke(null, null);
+            }
+        }
+
+        public static void Assignment1()
+        {
+            Sauna sauna = new Sauna();
+
+            int ticks = 0;
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            return;
+                        case ConsoleKey.N:
+                            sauna.TurnOn();
+                            break;
+                        case ConsoleKey.F:
+                            sauna.TurnOff();
+                            break;
+                    }
+                }
+                else
+                {
+                    ticks++;
+                    if (ticks % 100 == 0)
+                    {
+                        sauna.Update();
+                        Console.WriteLine("Temperature: " + Math.Round(sauna.TemperatureCelsius, 1) + "°C");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Basics/Objects/Sauna.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    public class Sauna
+    {
+        public double Temperature;
+        public double Humidity;
+        public bool IsOn { private set; get; }
+
+        public double TemperatureCelsius
+        {
+            get
+            {
+                return Temperature - 273.15;
+            }
+            set
+            {
+                Temperature = value + 273.15;
+            }
+        }
+        public bool IsOff { get { return !IsOn; } }
+
+        public Sauna()
+        {
+            TemperatureCelsius = 22.0;
+            Humidity = 0;
+        }
+
+        public void Update()
+        {
+            if (IsOn && TemperatureCelsius < 100)
+            {
+                Temperature += 0.01;
+            }
+            else if (IsOff && TemperatureCelsius > 20)
+            {
+                Temperature -= 0.01;
+            }
+        }
+
+        public void TurnOn()
+        {
+            IsOn = true;
+        }
+
+        public void TurnOff()
+        {
+            IsOn = false;
+        }
+    }
+}
