927108527f4ffb7d302f6d9d2017b90447706a73
VilRan <VilleMattiRantala@gmail.com>
2016-01-28 15:53:05 +0200

    Add more to Stuff

--- Assignments/Inheritance/Stuff.cs
+++ Assignments/Inheritance/Stuff.cs
@@ -19,31 +19,38 @@ namespace Inheritance
         double Height { get; }
     }
 
-    class Box : IBox
+    abstract class BoxBase : IBox
     {
-        public double Width { get { return width; } }
-        public double Length { get { return length; } }
-        public double Height { get { return height; } }
-        public double TopArea { get { return width * height; } }
-        public double Area { get { return 2 * width * length + 2 * width * height + 2 * length * height; } }
+        public abstract double Width { get; }
+        public abstract double Length { get; }
+        public abstract double Height { get; }
+        public double TopArea { get { return Width * Length; } }
+        public double LengthwiseArea { get { return Length * Height; } }
+        public double WidthwiseArea { get { return Width * Height; } }
+        public double Area { get { return 2 * TopArea + 2 * LengthwiseArea + 2 * WidthwiseArea; } }
         public double Volume { get { return Width * Height * Length; } }
+    }
 
-        private double width, length, height;
-
+    class Box : BoxBase
+    {
+        public override double Width { get { return width; } }
+        public override double Length { get { return length; } }
+        public override double Height { get { return height; } }
+        
         public Box(double width, double length, double height)
         {
             this.width = width;
             this.length = length;
             this.height = height;
         }
+
+        private readonly double width, length, height;
     }
     
     class ElectronicDevice : Box
     {
         public bool IsOn { get { return isOn; } }
-
-        private bool isOn;
-
+        
         public ElectronicDevice(double width, double length, double height)
             : base (width, length, height)
         {
@@ -54,81 +61,203 @@ namespace Inheritance
         {
             isOn = !isOn;
         }
+
+        private bool isOn;
     }
 
-    class Phone : ElectronicDevice
+    class Computer : ElectronicDevice
     {
-        public Phone(double width, double length, double height)
+        public Computer(double width, double length, double height)
             : base (width, length, height)
         {
 
         }
+
+        protected DataStorage hardDrive;
     }
 
-    class Tablet : ElectronicDevice
+    class Phone : Computer
+    {
+        public Phone(double width, double length, double height)
+            : base (width, length, height)
+        {
+            hardDrive = new DataStorage(16000);
+        }
+    }
+
+    class Tablet : Computer
     {
         public Tablet(double width, double length, double height)
             : base(width, length, height)
         {
-
+            hardDrive = new DataStorage(16000);
         }
     }
 
-    class Laptop : ElectronicDevice
+    class Laptop : Computer
     {
         public Laptop(double width, double length, double height)
             : base(width, length, height)
         {
+            discDrive = new OpticalDiscDrive();
+            hardDrive = new DataStorage(512000);
+        }
+
+        private OpticalDiscDrive discDrive;
+    }
+
+    class OpticalDiscDrive
+    {
+        private OpticalDisc disc;
+
+        private bool Insert(OpticalDisc disc)
+        {
+            if (disc == null)
+            {
+                this.disc = disc;
+                return true;
+            }
+            return false;
+        }
+
+        private OpticalDisc Eject()
+        {
+            OpticalDisc ejected = disc;
+            disc = null;
+            return disc;
+        }
+    }
+
+    class Cylinder : I3DShape
+    {
+        public double Diameter { get { return radius * 2; } }
+        public double Radius { get { return radius; } }
+        public double Circumference { get { return 2 * Math.PI * radius; } }
+        public double TopArea { get { return Math.PI * radius * radius; } }
+        public double SideArea { get { return Circumference * height; } }
+        public double Area { get { return 2 * TopArea + SideArea; } }
+        public double Volume { get { return TopArea * height; } }
+        
+        public Cylinder(double radius, double height)
+        {
+            this.radius = radius;
+            this.height = height;
+        }
+
+        protected readonly double radius;
+        protected readonly double height;
+    }
+
+    abstract class OpticalDisc : Cylinder
+    {
+        public DataStorage Data;
+        public abstract int MaxBytes { get; }
+
+        public OpticalDisc(double radius, double thickness)
+            : base(radius, thickness)
+        {
+            Data = new DataStorage(MaxBytes);
+        }
+    }
+
+    class CD : OpticalDisc
+    {
+        public override int MaxBytes { get { return 700; } }
+
+        public CD(int bytes)
+            : base(0.06, 0.0012)
+        {
 
         }
     }
 
-    class PaperDevice : IBox
+    class DVD : OpticalDisc
     {
-        public PaperSheet[] Pages;
+        public override int MaxBytes { get { return 4500; } }
 
-        public virtual double Width { get { return Pages.Max(p => p.Width); } }
-        public virtual double Length { get { return Pages.Max(p => p.Length); } }
-        public virtual double Height { get { return Pages.Sum(p => p.Height); } }
-        public double Area { get { return 2 * Width * Length + 2 * Width * Height + 2 * Length * Height; } }
-        public double Volume { get { return Width * Length * Height; } }
-
-        public PaperDevice(params PaperSheet[] pages)
+        public DVD(int bytes)
+            : base(0.06, 0.0012)
         {
-            Pages = pages;
+
+        }
+    }
+
+    class DataStorage
+    {
+        public DataStorage(int bytes)
+        {
+            data = new byte[bytes];
+        }
+
+        public void WriteString(int position, string str)
+        {
+            foreach (char c in str)
+            {
+                foreach (byte b in BitConverter.GetBytes(c))
+                {
+                    if (position >= data.Length)
+                        position -= data.Length;
+                    data[position] = b;
+                    position++;
+                }
+            }
+        }
+
+        public string ReadString(int position, int length)
+        {
+            StringBuilder str = new StringBuilder();
+            for (int i = position; i < position + length * 2; i += 2)
+                str.Append(BitConverter.ToChar(data, i));
+            return str.ToString();
+        }
+
+        private byte[] data;
+    }
+
+    class PaperDevice : BoxBase
+    {
+        public PaperSheet[] Sheets;
+
+        public override double Width { get { return Sheets.Max(p => p.Width); } }
+        public override double Length { get { return Sheets.Max(p => p.Length); } }
+        public override double Height { get { return Sheets.Sum(p => p.Height); } }
+        public int PageCount { get { return Sheets.Length * 2; } }
+
+        public PaperDevice(params PaperSheet[] sheet)
+        {
+            Sheets = sheet;
         }
     }
 
     class PaperSheet : Box
     {
-        public string Content;
+        public string LeftContent = "";
+        public string RightContent = "";
 
         public static PaperSheet A4 { get { return new PaperSheet(0.297, 0.210, 0.00005); } }
         public static PaperSheet A5 { get { return new PaperSheet(0.210, 0.148, 0.00005); } }
 
-        public PaperSheet(double width, double length, double thickness, string content = "")
+        public PaperSheet(double width, double length, double thickness)
             : base(width, length, thickness)
         {
-            Content = content;
+
         }
     }
 
     class Book : PaperDevice
     {
-        private Cover frontCover;
-        private Cover backCover;
-
-        public Cover FrontCover { get { return frontCover; } }
-        public Cover BackCover { get { return backCover; } }
-        public override double Width { get { return Math.Max(Math.Max(base.Width, frontCover.Width), backCover.Width); } }
-        public override double Length { get { return Math.Max(Math.Max(base.Length, frontCover.Length), backCover.Length); } }
-        public override double Height { get { return Math.Max(Math.Max(base.Height, frontCover.Height), backCover.Height); } }
+        public Cover FrontCover;
+        public Cover BackCover;
+        
+        public override double Width { get { return Math.Max(Math.Max(base.Width, FrontCover.Width), BackCover.Width); } }
+        public override double Length { get { return Math.Max(Math.Max(base.Length, FrontCover.Length), BackCover.Length); } }
+        public override double Height { get { return Math.Max(Math.Max(base.Height, FrontCover.Height), BackCover.Height); } }
 
         public Book(double coverThickness, params PaperSheet[] pages)
             : base(pages)
         {
-            frontCover = new Cover(Width, Height, coverThickness);
-            backCover = new Cover(Width, Height, coverThickness);
+            FrontCover = new Cover(Width, Height, coverThickness);
+            BackCover = new Cover(Width, Height, coverThickness);
         }
 
         public class Cover : Box
@@ -136,7 +265,6 @@ namespace Inheritance
             public Cover(double width, double lenght, double thickness)
                 : base(width, lenght, thickness)
             {
-
             }
         }
     }
@@ -148,68 +276,6 @@ namespace Inheritance
             : base(pages)
         {
 
-        }
-    }
-
-    class Disk : I3DShape
-    {
-        public double Diameter
-        {
-            get { return radius * 2; }
-        }
-        public double Radius
-        {
-            get { return radius; }
-        }
-        public double Circumference
-        {
-            get { return 2 * Math.PI * radius; }
-        }
-        public double Area
-        {
-            get { return Math.PI * radius * radius; }
-        }
-        public double Volume
-        {
-            get { return Area * thickness; }
-        }
-
-        protected readonly double radius;
-        protected readonly double thickness;
-
-        public Disk(double radius, double thickness)
-        {
-            this.radius = radius;
-            this.thickness = thickness;
-        }
-    }
-
-    class OpticalDisc : Disk
-    {
-        protected byte[] data;
-
-        public OpticalDisc(double radius, double thickness)
-            : base (radius, thickness)
-        {
-
-        }
-    }
-
-    class CD : OpticalDisc
-    {
-        public CD(int bytes)
-            : base(0.06, 0.0012)
-        {
-            data = new byte[bytes];
-        }
-    }
-
-    class DVD : OpticalDisc
-    {
-        public DVD(int bytes)
-            : base(0.06, 0.0012)
-        {
-            data = new byte[bytes];
         }
     }
 }
