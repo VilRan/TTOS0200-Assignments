de89c20e651fd8f0d1c19fad1b5903f5cc250353
VilRan <VilleMattiRantala@gmail.com>
2016-01-28 17:15:27 +0200

    Add more Universe stuff

---
+++ Assignments/Composition/RandomExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition
+{
+    static class RandomExtensions
+    {
+        public static double NextDouble(this Random random, double minValue, double maxValue)
+        {
+            return minValue + random.NextDouble() * (maxValue - minValue);
+        }
+
+        public static double NextGaussian(this Random random, double mean, double deviation)
+        {
+            double rn1 = random.NextDouble();
+            double rn2 = random.NextDouble();
+            double transform = Math.Sqrt(-2.0 * Math.Log(rn1)) * Math.Sin(2.0 * Math.PI * rn2);
+            double result = mean + deviation * transform;
+            return result;
+        }
+    }
+}
--- Assignments/Composition/Universe.cs
+++ Assignments/Composition/Universe.cs
@@ -11,18 +11,59 @@ namespace Composition
         public const double AstronomicalUnit = 149597870700.0;
         public const double Lightyear = 9460730472580800.0;
         public const double Parsec = 648000 / (Math.PI * AstronomicalUnit);
+        public const double EarthMass = 5.97237e24;
+        public const double JupiterMass = 1.8986e27;
+        public const double SunMass = 1.98855e30;
 
         public List<Galaxy> Galaxies = new List<Galaxy>();
+
+        public Universe(Random random)
+        {
+            int number = (int)Math.Abs(random.NextGaussian(0, 1000));
+            Galaxies = new List<Galaxy>(number);
+            for (int i = 0; i < number; i++)
+            {
+                Galaxy galaxy = new Galaxy(random);
+                Galaxies.Add(galaxy);
+            }
+        }
     }
 
     class Galaxy
     {
-        public List<CelestialBody> Bodies = new List<CelestialBody>();
+        public List<CelestialSystem> Systems;
+
+        public Galaxy(Random random)
+        {
+            int number = (int)Math.Abs(random.NextGaussian(0, 1000));
+            Systems = new List<CelestialSystem>(number);
+            for (int i = 0; i < number; i++)
+            {
+                CelestialSystem system = new CelestialSystem(random);
+                Systems.Add(system);
+            }
+        }
+    }
+
+    class CelestialSystem
+    {
+        public List<CelestialBody> Bodies;
+
+        public CelestialSystem(Random random)
+        {
+            int stars = (int)Math.Abs(random.NextGaussian(0, 1));
+            Bodies = new List<CelestialBody>(stars);
+            for (int i = 0; i < stars; i++)
+            {
+                Star star = new Star(random);
+                Bodies.Add(star);
+            }
+        }
     }
 
     class CelestialBody
     {
-        public List<CelestialBody> Satellites = new List<CelestialBody>();
+        public List<CelestialBody> Satellites;
         public double Periapsis;
         public double Apoapsis;
         public double Mass;
@@ -31,33 +72,110 @@ namespace Composition
         public double ApoapsisInAUs { get { return Apoapsis / Universe.AstronomicalUnit; } }
         public double PeriapsisInLightyears { get { return Periapsis / Universe.Lightyear; } }
         public double ApoapsisInLightyears { get { return Apoapsis / Universe.Lightyear; } }
-        public double MassInEarths { get { return Mass / (5.97237 * Math.Pow(10, 24)); } }
-        public double MassInJupiters { get { return Mass / (1.8986 * Math.Pow(10, 27)); } }
-        public double MassInSuns { get { return Mass / (1.98855 * Math.Pow(10, 30)); } }
+        public double MassInEarths { get { return Mass / Universe.EarthMass; } }
+        public double MassInJupiters { get { return Mass / Universe.JupiterMass; } }
+        public double MassInSuns { get { return Mass / Universe.SunMass; } }
+
+        public CelestialBody(Random random)
+        {
+            int numSatellites = (int)Math.Abs(random.NextGaussian(0, 10));
+            Satellites = new List<CelestialBody>(numSatellites);
+            for (int i = 0; i < numSatellites; i++)
+            {
+                CelestialBody moon = CreateFromMass(Mass * random.NextDouble(0, 0.1), random);
+                Satellites.Add(moon);
+            }
+        }
+
+        public static CelestialBody CreateFromMass(double mass, Random random)
+        {
+            if (mass > 1e29)
+                return new Star(random);
+            if (mass > 1e26)
+                return new GasGiant(random);
+            if (mass > 1e25)
+                return new IceGiant(random);
+            if (mass > 1e23)
+                return new RockyPlanet(random);
+            if (mass > 1e21)
+                return new DwarfPlanet(random);
+
+            return new Asteroid(random);
+        }
     }
 
     class Star : CelestialBody
     {
+        public Star(Random random)
+            : base (random)
+        {
+            Mass = Math.Abs(random.NextGaussian(0, Universe.SunMass));
+            int planets = (int)Math.Abs(random.NextGaussian(0, 10));
+            Satellites = new List<CelestialBody>(planets);
+            for (int i = 0; i < planets; i++)
+            {
 
+            }
+        }
     }
-
-    class Planet : CelestialBody
+    
+    abstract class Planet : CelestialBody
     {
+        public Planet(Random random)
+            : base(random)
+        {
 
+        }
     }
 
     class GasGiant : Planet
     {
+        public GasGiant(Random random)
+            : base(random)
+        {
 
+        }
     }
 
     class IceGiant : Planet
     {
+        public IceGiant(Random random)
+            : base(random)
+        {
 
+        }
     }
 
     class RockyPlanet : Planet
     {
+        public RockyPlanet(Random random)
+            : base(random)
+        {
+            int moons = (int)Math.Abs(random.NextGaussian(0, 2));
+            Satellites = new List<CelestialBody>(moons);
+            for (int i = 0; i < moons; i++)
+            {
+                CelestialBody moon = CreateFromMass(Mass * random.NextDouble(0.001, 0.1), random);
+                Satellites.Add(moon);
+            }
+        }
+    }
 
+    class DwarfPlanet : Planet
+    {
+        public DwarfPlanet(Random random)
+            : base(random)
+        {
+
+        }
+    }
+
+    class Asteroid : Planet
+    {
+        public Asteroid(Random random)
+            : base(random)
+        {
+
+        }
     }
 }
