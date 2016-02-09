82bd4b40899079ce1550cd00e485f6278b47121d
VilRan <VilleMattiRantala@gmail.com>
2016-02-09 19:01:39 +0200

    Add Recap Assignment2

---
+++ Assignments/Recap/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recap
+{
+    class Product
+    {
+        public string Type;
+        public double Price;
+
+        public override string ToString()
+        {
+            return Type + " " + Price + " e";
+        }
+    }
+}
--- Assignments/Recap/Program.cs
+++ Assignments/Recap/Program.cs
@@ -10,11 +10,10 @@ namespace Recap
     class Program
     {
         const int MinAssignments = 1;
-        const int MaxAssignments = 1;
+        const int MaxAssignments = 2;
 
         static void Main(string[] args)
         {
-            Assignment1();
             while (true)
             {
                 Console.Write("Enter assignment number (" + MinAssignments + "-" + MaxAssignments + ") or enter something else to exit > ");
@@ -43,6 +42,20 @@ namespace Recap
             for (int i = 1; i <= 6; i++)
                 Console.WriteLine("- " + i + " count is " + die.GetTimes(i));
             Console.WriteLine();
+        }
+
+        public static void Assignment2()
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product() { Type = "Milk", Price = 1.4 });
+            products.Add(new Product() { Type = "Beer", Price = 2.2 });
+            products.Add(new Product() { Type = "Butter", Price = 3.2 });
+            products.Add(new Product() { Type = "Cheese", Price = 4.2 });
+
+            Console.WriteLine("All products in collection: ");
+            foreach (Product product in products)
+                Console.WriteLine("- product : " + product);
         }
     }
 }
