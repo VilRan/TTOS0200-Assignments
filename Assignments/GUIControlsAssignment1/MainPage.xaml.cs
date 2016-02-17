de5590169bbe10d4bebad9e012204e4921324f17
VilRan <VilleMattiRantala@gmail.com>
2016-02-17 19:27:56 +0200

    Add GUIControls Assignments 1 and 2

---
+++ Assignments/GUIControlsAssignment1/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GUIControlsAssignment1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "";
+            CheckBox[] checkboxes = { checkBox, checkBox1, checkBox2, checkBox3, checkBox4 };
+            foreach (CheckBox check in checkboxes)
+            {
+                if (check.IsChecked.Value)
+                {
+                    textBox.Text += check.Content + " ";
+                }
+            }
+        }
+    }
+}
